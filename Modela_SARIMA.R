27566d1178ded6468ecd3a6f9d07ea130c54353b
Carlo <carlo.demusis@gmail.com>
2023-04-18 18:36:46 -0400

    Add files via upload

--- Modela_SARIMA.R
+++ Modela_SARIMA.R
@@ -2,14 +2,30 @@ library(readr)
 library(fUnitRoots)
 library(tseries)
 library(forecast)
+library(ggplot2)
+library(readr)
+library(readxl)
+library(xts)
 
-dados <- Geracao_FV_EMT_2014_2022
+setwd("~/Lutero")
+
+na_var <- "Consumo de Energia Elétrica (MWh)"
+
+dados <- read_excel("consumo.xlsx", col_types = c("text", 
+                                                  "numeric", "numeric", "numeric", "numeric", 
+                                                  "numeric", "numeric", "numeric", "numeric"))
+
+dados$data <- as.Date(paste(dados$Ano, 
+                            dados$Mes, 
+                            1, 
+                            sep = "-"))
 
 # Cria objeto Time Series
-ST <- ts(dados$Pot_Ins_kWp, 
-              start=c(2015, 8), 
-              f=12)
-autoplot(ST)
+ST <- as.xts(ts(dados[, na_var], 
+              start=c(2003, 1), 
+              f=12))
+colnames(ST) <- c('CEE')
+plot(ST)
 
 # Testes da estacionariedade da série
 # KPSS
@@ -26,30 +42,58 @@ adf.test(ST, alternative="stationary")
 acf(ST)
 pacf(ST)
 
-# Se rejeitarmos a hipótese da estacionariedade, busca-se o valor de d.
-# Calculando as correlações para uma diferença de ordem 1
-acf(diff(ST, 
-         differences=1))
-pacf(diff(ST, 
-          differences=1))
-
 # Busca dos parâmetros do SARIMA com base na otimização do AIC
 
 # Transformação de BOX-COX
-# l <- BoxCox.lambda(ST)
-
+l <- BoxCox.lambda(ST)
 ajuste <- auto.arima(ST, 
-#                     lambda = l, 
+                     lambda = l, 
                      allowdrift = TRUE, 
                      allowmean = TRUE, 
-                     seasonal.test = "seas",
-                     stepwise = FALSE,
+                     seasonal = TRUE,
+                     stepwise = TRUE,
                      approximation = FALSE,
                      trace = TRUE
 ) 
 ajuste
 
-previsao <- forecast(ajuste, h = 5)
+previsao <- forecast(ajuste, h = 12)
+plot(previsao)
+
+
 autoplot(previsao, xlab = 'Data decimal', ylab = 'Consumo mensal')
 
 
+# Plote o gráfico com a série original e estimativas
+previsoes <- as.xts(as.data.frame(previsao$mean), 
+                    order.by = seq(from = as.Date("2023-01-01"), 
+                                   to = as.Date("2023-12-01"), 
+                                   by = "month"))
+colnames(previsoes) <- c(na_var)
+
+valor_estimado <- as.xts(as.data.frame(ajuste$fitted), 
+                           order.by = as.Date(dados$data))
+colnames(valor_estimado) <- c("na_var")
+
+valor_est_obs <- cbind(ST, 
+                       valor_estimado, 
+                       previsoes)
+
+valor_est_obs<- consumo_est_obs["2005/"]
+
+plot(valor_est_obs$CEE, 
+     main = "Série original, estimativas e previsões", 
+     xlab = "Data", 
+     ylab = na_var, 
+     col = "blue", 
+     lwd = 1, 
+     type = "l", 
+     xaxt = "n")
+lines(valor_est_obs$CEE.Estimado, 
+      col = "red", 
+      lwd = 1, 
+      lty = 5)
+lines(valor_est_obs$CEE.Estimado.1, 
+      col = "green", 
+      lwd = 1, 
+      lty = 2)
